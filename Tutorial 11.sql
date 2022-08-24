6cdfc14a39c19df601c4b8cac7c6340612ab107e
Chetan  Salunke <90956042+Chetan3520@users.noreply.github.com>
2022-08-24 21:20:43 +0530

    Add files via upload

---
+++ Tutorial 11.sql
@@ -0,0 +1,14 @@
+# NORMALIZATION OF DATABASE
+/* 
+-- Normalization is a database design technique that organizes tables in a 
+manner that reduces redundancy(useless) and dependency of data.
+-- To avoid insertion update & deletion anomaly.
+-- Normalization divideas larger tables into smaller tables and links them using relationships(primary key,foreign key).
+*/
+
+
+# SNOWFLAKE & BRIDGING
+# SNOWFLAKE- We are collecting tables throughout what we called snowflake 
+
+
+
