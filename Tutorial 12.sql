3f8d58cf927d2b8cd05694403f4d679f9ab505f1
Chetan  Salunke <90956042+Chetan3520@users.noreply.github.com>
2022-08-24 21:21:23 +0530

    Add files via upload

---
+++ Tutorial 12.sql
@@ -0,0 +1,20 @@
+#HOW TO IMPORT DATASET IN THE SQL?
+/*
+click on schema which left side of the window in and down side of Nevigator.
+write click on the table of the database where you want to import the file.
+Select 'table data import wizard'
+Select your file from the browse.
+then click on contunue and go on and you can change the some configuration as per your requirement in the journey.
+then finish.
+
+
+*/
+
+show databases; 
+use new_schema;
+select * from income_spend;  
+select income from income_spend where income>400;
+
+
+
+
